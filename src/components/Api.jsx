5cd2347c00de987d7c5ca2677398871c2035c3cd
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-08 15:50:14 -0400

    added api.jsx

---
+++ src/components/Api.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class Api extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoaded: false,
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("http://myapi-profstream.herokuapp.com/api/916e0a/persons")
+            .then(res => {
+                const wineAPI = res.data;
+                console.log('wine list --->', wineAPI)
+
+                this.setState({ data: wineAPI })
+            })
+
+            .catch(error => {
+                console.log('there is an error', error)
+            })
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Api;
