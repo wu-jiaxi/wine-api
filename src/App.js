c77e26cb61982bdacb82879df847db90180bbb25
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-13 16:48:42 -0400

    adding route lists

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ import React, { Component } from 'react';
 import Api from './components/Api'
 import './App.css';
 import Winelist from './components/Winelist'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 class App extends Component {
   state = {}
   render() {
-    return (<div>
-      <Api name={this.state.id} />
-      <Winelist />
-    </div>);
+    return (
+      <Router> 
+        <div className="App">
+          <Api />
+          <Route path="/about"
+          <Winelist />
+        </div>
+      </Router>);
   }
 }
 
